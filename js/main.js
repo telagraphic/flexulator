ff20e497d8343d1eb435cff741687ec751b6412b
telagraphic <telagraphic@gmail.com>
2018-11-08 11:14:09 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -33,8 +33,7 @@ function updateContainerWidth() {
 function updateItemWidth() {
   var flexItems = Array.prototype.slice.call(document.getElementsByClassName('flex-item'));
   flexItems.forEach(function (item) {
-    console.log(item);
-    item.lastElementChild.textContent = item.offsetWidth;
+    item.firstElementChild.textContent = item.offsetWidth;
   });
 }
 
@@ -52,41 +51,101 @@ function addFlexItem() {
   length++;
 
   // get flex styles
-
   var grow = document.getElementById('flex-grow').value;
   var shrink = document.getElementById('flex-shrink').value;
   var basis = document.getElementById('flex-basis').value;
-  console.log(basis);
 
   // create new element with flex values
 
   var flexItem = document.createElement('section');
-  var itemContent = document.createElement('h4');
-  itemContent.setAttribute('class', 'item-content');
-  itemContent.textContent = 'flex: ' + grow + ' ' + shrink + ' ' + basis;
+  flexItem.setAttribute('id', 'flex-item-' + length);
+  flexItem.setAttribute('class', 'flex-item');
+
+  var flexValues = document.createElement('section');
+  flexValues.setAttribute('class', 'item-values');
+
+  var growInput = document.createElement('input');
+  growInput.setAttribute('type', 'number');
+  growInput.setAttribute('class', 'item-grow');
+  growInput.setAttribute('value', grow);
+
+  var shrinkInput = document.createElement('input');
+  shrinkInput.setAttribute('type', 'number');
+  shrinkInput.setAttribute('class', 'item-shrink');
+  shrinkInput.setAttribute('value', shrink);
+
+  var basisInput = document.createElement('input');
+  basisInput.setAttribute('type', 'text');
+  basisInput.setAttribute('class', 'item-basis');
+  basisInput.setAttribute('value', basis);
+
+  flexValues.appendChild(growInput);
+  flexValues.appendChild(shrinkInput);
+  flexValues.appendChild(basisInput);
+
+  // flexItem.style.display = 'flex';
+  flexItem.style.flexGrow = grow;
+  flexItem.style.flexShrink = shrink;
+  flexItem.style.flexBasis = basis + 'px';
+
 
   var itemWidth = document.createElement('h4');
   itemWidth.setAttribute('class', 'item-width');
   itemWidth.textContent = '100';
 
-  flexItem.setAttribute('class', 'flex-item');
-  flexItem.setAttribute('id', 'flex-item-' + length);
-  // flexItem.style.display = 'flex';
-  flexItem.style.flexGrow = grow;
-  flexItem.style.flexShrink = shrink;
-  flexItem.style.flexBasis = basis + 'px';
-  flexItem.appendChild(itemContent);
+  var button = document.createElement('button');
+  button.setAttribute('class', 'remove-item-button');
+  button.textContent = 'X';
+
   flexItem.appendChild(itemWidth);
+  flexItem.appendChild(flexValues);
+  flexItem.appendChild(button);
+
 
   // add to flex-container
   flexContainer.appendChild(flexItem);
 
   updateItemWidth();
+  editFlexItem();
+  removeFlexItem();
+};
+
+function editFlexItem() {
+
+  var flexItems = Array.prototype.slice.call(document.getElementsByClassName('flex-item'));
+  flexItems.forEach(function (item) {
+    item.addEventListener('input', function (event) {
+      var parent = event.target.parentElement.parentElement;
+      if (event.target.matches('.item-grow')) {
+        parent.style.flexGrow = event.target.value;
+      } else if (event.target.matches('.item-shrink')) {
+        parent.style.flexShrink = event.target.value;
+      } else if (event.target.matches('.item-basis')) {
+        parent.style.flexBasis = event.target.value + 'px';
+      }
+
+      updateItemWidth();
+    });
+  });
+
 }
 
 function removeFlexItem() {
-  // get flex item clicked and remove
 
-  //
+  var flexItems = Array.prototype.slice.call(document.getElementsByClassName('flex-item'));
+  flexItems.forEach(function (item) {
+    console.log(item);
+    item.addEventListener('click', function (event) {
+      // var parent = event.target.parentElement.parentElement;
+      if (event.target.matches('.remove-item-button')) {
+        event.target.parentElement.remove();
+      }
+
+      updateItemWidth();
+    });
+  });
 
 }
+
+editFlexItem();
+removeFlexItem();
