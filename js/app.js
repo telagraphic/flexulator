c813eef1a0960756196c28550c597c36d07cd73c
telagraphic <telagraphic@gmail.com>
2020-07-17 08:02:41 -0400

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
+let select = e => document.querySelector(e);
+let selectAll = e => document.querySelectorAll(e);
+
+const flexContainer = {
+  container:0,
+  totalFlexBasis:0,
+  remainingSpace:0
+}
+
+const flexItem = {
+  properties: {
+    width: 500,
+    grow: 1,
+    shrink: 1,
+    basis: 100
+  },
+  formula: {
+    grow: {
+      itemValue:0,
+      total:0,
+      remainingFlexSpace:0,
+      itemSpace:0
+    },
+    shrink: {
+      itemValue:0,
+      basis:0,
+      totalFlexBasis:0,
+      value:0,
+      remainingFlexSpace:0,
+      itemSpace:0
+    }
+  }
+}
+
+const flexItems = [];
+
 var flexContainer = document.getElementById('flex-container');
 var addFlexItemButton = document.getElementById('add-flex-item-button');
 var showTotalFlexBasis = document.getElementById('total-flex-basis');
