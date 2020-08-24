1f93e23e8486298f8105fc0b763bbd6d4a146032
telagraphic@gmail.com <telagraphic@gmail.com>
2020-08-24 13:49:59 -0400

    ✒️ TYPE/FONT: subset web fonts, cut size in half

---
+++ fonts/ttf/subset.sh
@@ -0,0 +1,8 @@
+pyftsubset "Consolas-Bold.ttf" --output-file="../subset/Consolas-Bold.woff" --flavor=woff --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Consolas-Bold.ttf" --output-file="../subset/Consolas-Bold.woff2" --flavor=woff2 --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Consolas.ttf" --output-file="../subset/Consolas.woff" --flavor=woff --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Consolas.ttf" --output-file="../subset/Consolas.woff2" --flavor=woff2 --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Roboto-Regular.ttf" --output-file="../subset/Roboto-Regular.woff" --flavor=woff --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Roboto-Regular.ttf" --output-file="../subset/Roboto-Regular.woff2" --flavor=woff2 --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Roboto-Thin.ttf" --output-file="../subset/Roboto-Thin.woff" --flavor=woff --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
+pyftsubset "Roboto-Thin.ttf" --output-file="../subset/Roboto-Thin.woff2" --flavor=woff2 --layout-features="*" --no-hinting --desubroutinize --unicodes=U+0-10FFFF
